fdf17fec398e6c0322528108d795644947cfd6fd
Praneeth Reddy Araka <praraka@mix.wvu.edu>
2017-09-27 19:22:29 -0400

    Update BookList.js

--- src/BookList.js
+++ src/BookList.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import BookShelf from './BookShelf'
+import { Link } from 'react-router-dom'
+import BookShelf from './BookShelf.js'
 
 function BookList (props) {
-
-    return (
-    <div className="list-books">
-     <div className="list-books-title">
-       <h1>My Reads</h1>
-     </div>
-     <div className="list-books-content">
-      <BookShelf
-      		books={props.books.filter(book=>book.shelf==='currentlyReading')}
+        return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1> </div>
+            <div className="list-books-content">
+                <BookShelf
+                    books={props.books.filter(book=>book.shelf==='currentlyReading')}
                     shelf="Currently Reading"
+                    onHandleChange={props.onHandleChange}
                 />
                 <BookShelf
                     books={props.books.filter(book=>book.shelf==='wantToRead')}
                     shelf="Want to Read"
+                    onHandleChange={props.onHandleChange}
                 />
                 <BookShelf
                     books={props.books.filter(book=>book.shelf==='read')}
                     shelf="Read"
+                    onHandleChange={props.onHandleChange}
                 />
             </div>
             <div className="open-search">
@@ -28,5 +30,6 @@ function BookList (props) {
         </div>
         )    
 }
+
 
 export default BookList
