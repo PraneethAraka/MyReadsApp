692536f254ff1ccf2804ef9ddf6e45906526c57e
Praneeth Reddy Araka <praraka@mix.wvu.edu>
2017-10-04 19:34:25 -0400

    Update BookShelf.js

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -15,6 +15,7 @@ import React from 'react'
                                 <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                                 <div className="book-shelf-changer">
                                     <select value={book.shelf}>
+                                        onChange={(event) => props.onHandleChange(book, event.target.value)}>
                                         <option value="" disabled>Move to...</option>
                                         <option value="currentlyReading">Currently Reading</option>
                                         <option value="wantToRead">Want to Read</option>
